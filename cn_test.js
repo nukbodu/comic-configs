790921872be9b0b9fd37764841ea98118da0ba3e
nukbodu <nukbodu@gmail.com>
2025-05-28 00:31:03 +0800

    Create cn_test.js

---
+++ cn_test.js
@@ -0,0 +1,40 @@
+[
+    {
+        "name": "拷贝漫画",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/copy_manga.js",
+        "key": "copy_manga",
+        "version": "1.1.5"
+    },
+    {
+        "name": "Komiic",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/komiic.js",
+        "key": "Komiic",
+        "version": "1.0.2"
+    },
+    {
+        "name": "包子漫画",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/baozi.js",
+        "key": "baozi",
+        "version": "1.0.4"
+    },
+    {
+        "name": "禁漫天堂",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/jm.js",
+        "key": "jm",
+        "version": "1.1.4",
+        "description": "禁漫天堂漫畫源, 不能使用時請嘗試切換分流"
+    },
+    {
+        "name": "MangaDex",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/manga_dex.js",
+        "key": "manga_dex",
+        "version": "1.0.0",
+        "description": "Account feature is not supported yet."
+    },
+    {
+        "name": "爱看漫",
+        "fileName": "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/ikmmh.js",
+        "key": "ikmmh",
+        "version": "1.0.2"
+    }
+]
